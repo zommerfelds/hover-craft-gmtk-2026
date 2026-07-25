29abb5b804779fc09b538801f042bac927ce9ccf
zommerfelds <christian.zommerfelds@gmail.com>
2026-07-25 14:44:52 +0200

    new control idea

--- player.gd
+++ player.gd
@@ -3,6 +3,9 @@ extends RigidBody3D
 const stabilizer_y_pid_params = preload("res://pid_params.tres")
 @onready var pid_controller = PidController.new(stabilizer_y_pid_params)
 
+var time_acc = 0.0
+var thrust_ramp = 0.0
+
 func _ready() -> void:
 	pass
 
@@ -11,7 +14,12 @@ func _process(delta: float) -> void:
 		get_tree().reload_current_scene()
 
 func _physics_process(delta: float) -> void:
-	linear_velocity.x = 3
+	%RayCast3D.position = position
+
+	time_acc += delta
+	rotation_degrees.z = sin(time_acc * 5) * 30
+
+	# linear_velocity.x = 3
 
 	var dist = 10.0
 	if %RayCast3D.is_colliding():
@@ -27,6 +35,11 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("thrust") and dist > 0.0:
 		#var thrust = 200 * max(0, 1-dist) + 5
 		#apply_force(Vector3(0, thrust, 0))
-		thrust = max(thrust, 30)
+		# thrust = max(thrust, 30)
+		thrust_ramp = min(1, thrust_ramp + delta * 4)
+		apply_force(global_transform.basis * Vector3(0, 30, 0) * thrust_ramp)
+	else:
+
+		thrust_ramp = max(0, thrust_ramp - delta * 5)
 
 	apply_force(Vector3(0, thrust, 0))
