1cd4315e15ef005ab0a39996fbd0a08519552673
zommerfelds <christian.zommerfelds@gmail.com>
2026-07-26 14:37:28 +0200

    dual thrusters

--- player.gd
+++ player.gd
@@ -5,8 +5,10 @@ const stabilizer_y_pid_params = preload("res://pid_params.tres")
 
 var time_acc = 0.0
 var thrust_ramp = 0.0
+var flip = false
 
 func _ready() -> void:
+	update_active_cylinder()
 	pass
 
 func _process(delta: float) -> void:
@@ -17,9 +19,10 @@ func _physics_process(delta: float) -> void:
 	%RayCast3D.position = position
 
 	time_acc += delta
-	rotation_degrees.z = sin(time_acc * 5) * 30
 
-	# linear_velocity.x = 3
+	if Input.is_action_just_released("thrust"):
+		flip = !flip
+		update_active_cylinder()
 
 	var dist = 10.0
 	if %RayCast3D.is_colliding():
@@ -27,18 +30,25 @@ func _physics_process(delta: float) -> void:
 		dist = %RayCast3D.global_position.distance_to(hit_point)
 
 	var thrust = 0
-
 	var pid_output = pid_controller.calculate(1, dist, delta)
-	print(pid_output, " ", dist)
+	#print(pid_output, " ", dist)
 	thrust = max(0, 30 * pid_output)
+	apply_force(Vector3(0, thrust, 0))
 
+	var particles = %ParticlesRight if flip else %ParticlesLeft
 	if Input.is_action_pressed("thrust") and dist > 0.0:
-		#var thrust = 200 * max(0, 1-dist) + 5
-		#apply_force(Vector3(0, thrust, 0))
-		# thrust = max(thrust, 30)
 		thrust_ramp = min(1, thrust_ramp + delta * 4)
-		apply_force(global_transform.basis * Vector3(0, 30, 0) * thrust_ramp)
+		var dir = particles.global_basis.y
+		apply_force(global_transform.basis * 30 * dir * thrust_ramp)
+		particles.amount_ratio = thrust_ramp
 	else:
 		thrust_ramp = max(0, thrust_ramp - delta * 5)
-	%GPUParticles3D.amount_ratio = thrust_ramp
-	apply_force(Vector3(0, thrust, 0))
+		%ParticlesLeft.amount_ratio = 0
+		%ParticlesRight.amount_ratio = 0
+
+
+func update_active_cylinder():
+	%CSGCylinderLeft.material_override = null
+	%CSGCylinderRight.material_override = null
+	var active_cylinder = %CSGCylinderRight if flip else %CSGCylinderLeft
+	active_cylinder.material_override = preload("res://material_highlighted.tres")
