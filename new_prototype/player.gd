40a807b4f8b8f8e3f7627b722414c60f3c4b967f
zommerfelds <christian.zommerfelds@gmail.com>
2026-08-02 17:36:44 +0200

    hot air ballon style

--- new_prototype/player.gd
+++ new_prototype/player.gd
@@ -1,5 +1,8 @@
 extends RigidBody3D
 
+var up_force = 0.0
+var up_force_vel = 0.0
+
 func _ready() -> void:
 	gravity_scale = 0.2
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -9,10 +12,19 @@ func _physics_process(delta: float) -> void:
 		apply_force(Vector3(0, 8, 0))
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	apply_impulse(Vector3(0, up_force, 0))
+	up_force = max(0.0, up_force + up_force_vel * delta)
+	if up_force == 0.0:
+		up_force_vel = 0.0
+	else:
+		up_force_vel -= delta * 5.0
+	print("up_force_vel: ", up_force_vel, ", up_force:", up_force)
+
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
-		apply_impulse(Vector3(0, 2, 0))
+		# apply_impulse(Vector3(0, 2, 0))
+		up_force_vel += 1.0
 		print("Mouse Click/Unclick at: ", event.position, event.pressed)
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	elif event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
